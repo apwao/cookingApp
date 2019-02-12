3b12b468a05d76ec50f61257225474f2d5020eb5
Sowayi <sapwao@gmail.com>
2019-02-12 04:27:17 -0500

    Add function that converts litres to gallons

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,10 @@ return gallon * 3.78;
 let gallon = prompt("Enter amount in gallons:");
 let resultLitre = gallonToLitre(gallon);
 alert(resultLitre + " " + "Litres");
+//Convert litres to gallons
+let litreToGallon = function (litre) {
+return litre/3.78;
+};
+let litre = parseInt(prompt("Enter amount in litres:"));
+let resultGallon = litreToGallon(litre);
+alert(resultGallon + " " + "Gallons");
